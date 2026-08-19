4d7c40274f69959f9d0c72d9d1473f755ecbb946
Vikas Verma <vermasingh2002@gmail.com>
2026-08-19 13:24:11 +0530

    feat: Add server proxy configuration for API requests

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://aspgen.vaaptech.com:9000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  }
 })
